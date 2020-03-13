4e6a4d80fdc60e5b4b2d0882d9f52c6b4e76cd93
beratcan97 <beratcan_acil@hotmail.se>
2020-03-13 06:44:50 +0100

    project splited in components

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { BitcoinDataService } from "../../services/bitcoin-data.service";
-import { CurrencyConverterService } from "../../services/currency-converter.service";
 
 @Component({
   selector: "app-start",
@@ -8,32 +6,9 @@ import { CurrencyConverterService } from "../../services/currency-converter.serv
   styleUrls: ["./start.component.css"]
 })
 export class StartComponent implements OnInit {
-  isLoaded = false;
-  wikiRawDATA;
 
-  constructor(private bitcoinDataService: BitcoinDataService) {}
+  constructor() {}
 
   ngOnInit() {
-    this.getDATA();
-  }
-
-  getDATA(): void {
-    this.bitcoinDataService.getCoronaDataFromWikipediApi().subscribe(DATA => {
-
-
-      let s = DATA.query.pages[63239190].extract;
-      let htmlObject = document.createElement("div");
-      htmlObject.innerHTML = s;
-      const tmpRawDataString = htmlObject.getElementsByTagName("p")[5].innerHTML;
-      console.log(tmpRawDataString);
-
-      const rawDataString = tmpRawDataString.substring(
-        tmpRawDataString.lastIndexOf("2020, there are ") + 1, 
-        tmpRawDataString.lastIndexOf(" confirmed cases in Sweden, the plurality")
-      );
-      
-      const finalNumber = rawDataString.substring(15);
-      this.wikiRawDATA = finalNumber;
-    });
   }
 }
---
+++ src/app/components/stats-container/stats-container.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { BitcoinDataService } from "../../services/bitcoin-data.service";
+
+@Component({
+  selector: 'app-stats-container',
+  templateUrl: './stats-container.component.html',
+  styleUrls: ['./stats-container.component.css']
+})
+export class StatsContainerComponent implements OnInit {
+  isLoaded = false;
+  wikiRawDATA;
+
+  constructor(private bitcoinDataService: BitcoinDataService) {}
+
+  ngOnInit() {
+    this.getDATA();
+  }
+
+  getDATA(): void {
+    this.bitcoinDataService.getCoronaDataFromWikipediApi().subscribe(DATA => {
+
+
+      let s = DATA.query.pages[63239190].extract;
+      let htmlObject = document.createElement("div");
+      htmlObject.innerHTML = s;
+      const tmpRawDataString = htmlObject.getElementsByTagName("p")[5].innerHTML;
+      console.log(tmpRawDataString);
+
+      const rawDataString = tmpRawDataString.substring(
+        tmpRawDataString.lastIndexOf("2020, there are ") + 1, 
+        tmpRawDataString.lastIndexOf(" confirmed cases in Sweden, the plurality")
+      );
+      
+      const finalNumber = rawDataString.substring(15);
+      this.wikiRawDATA = finalNumber;
+    });
+  }
+}
