bb05f6a2bebd526be04fd65a805ed715a00231cd
beratcan97 <beratcan_acil@hotmail.se>
2020-03-14 03:04:16 +0100

    bug fix

--- src/app/components/stats-container/stats-container.component.ts
+++ src/app/components/stats-container/stats-container.component.ts
@@ -8,29 +8,27 @@ import { BitcoinDataService } from "../../services/bitcoin-data.service";
 })
 export class StatsContainerComponent implements OnInit {
   isLoaded = false;
-  wikiRawDATA = "769";
+  wikiRawDATA;
 
   constructor(private bitcoinDataService: BitcoinDataService) {}
 
   ngOnInit() {
-    // this.getDATA();
+    this.getDATA();
   }
 
   getDATA(): void {
     this.bitcoinDataService.getCoronaDataFromWikipediApi().subscribe(DATA => {
 
-
       let s = DATA.query.pages[63239190].extract;
       let htmlObject = document.createElement("div");
       htmlObject.innerHTML = s;
-      const tmpRawDataString = htmlObject.getElementsByTagName("p")[5].innerHTML;
-      console.log(tmpRawDataString);
+      const tmpRawDataString = htmlObject.getElementsByTagName("p")[4].innerHTML;
 
       const rawDataString = tmpRawDataString.substring(
         tmpRawDataString.lastIndexOf("2020, there are ") + 1, 
-        tmpRawDataString.lastIndexOf(" confirmed cases in Sweden, the plurality")
+        tmpRawDataString.lastIndexOf("confirmed cases")
       );
-      
+
       const finalNumber = rawDataString.substring(15);
       this.wikiRawDATA = finalNumber;
     });
