d11b7eb52220787e2276070a84b087fd7b597fa7
beratcan97 <beratcan_acil@hotmail.se>
2020-03-14 19:56:57 +0100

    projeect updated

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('bitcoinkollen app is running!');
+    expect(page.getTitleText()).toEqual('coronakollen app is running!');
   });
 
   afterEach(async () => {
